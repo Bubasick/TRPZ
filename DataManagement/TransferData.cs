ed9678aec9dc96ac9ee80e07d7ce45ad5b42a823
DanyloKam@gmail.com <DanyloKam@gmail.com>
2020-03-26 07:10:48 +0200

    Some Weird COnflict occured

---
+++ DataManagement/TransferData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Configuration;
+using System.IO;
+
+namespace DataManagement
+{
+    public static class TransferData
+    {
+        public static object Transfer(Type type)
+        {
+           string  connectionString = ConfigurationManager.ConnectionStrings["Path"].ConnectionString;
+            return new Serializator(new XMLSerializator(type)).Deserialize(connectionString);
+    }
+
+    }
+}
